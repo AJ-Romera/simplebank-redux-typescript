46a729464c304ee001f6e84b8721154210fb2167
AJ-Romera <ajromeraalfonso@gmail.com>
2021-08-12 17:13:21 +0200

    CHNG: Make & Able the Buttons to Deposit, Withdraw & Bankrupt

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ActionCreators, State } from './state';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const {depositMoney, withdrawMoney, bankrupt} = bindActionCreators(ActionCreators, dispatch)
+
+  const amount = useSelector((state: State) => state.bank)
   return (
     <main className="App">
-      <h1>Testing...</h1>
+      <h1>{amount}</h1>
+      <button onClick={() => depositMoney(100)}>Deposit</button>
+      <button onClick={() => withdrawMoney(200)}>Withdraw</button>
+      <button onClick={() => bankrupt()}>Bankrupt</button>
     </main>
   );
 }
